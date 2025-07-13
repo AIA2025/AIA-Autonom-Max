5b91d208a6f0b4aaf95d29f61b51374e07883744
Marjan Djordjevic <marjandjordjevic@iMac-von-Marjan.fritz.box>
2025-07-13 02:02:36 +0200

    Use dotenv for config, remove secrets from repo

--- config/__init__.py
+++ config/__init__.py
@@ -1,4 +1,16 @@
+from dotenv import load_dotenv
+import os
 
-import json
-with open("config/config.json", "r") as f:
-    config = json.load(f)
+load_dotenv()
+
+config = {
+    "telegram_token": os.getenv("TELEGRAM_TOKEN"),
+    "chat_id": os.getenv("CHAT_ID"),
+    "openai_api_key": os.getenv("OPENAI_API_KEY"),
+    "shopify_store": os.getenv("SHOPIFY_STORE"),
+    "shopify_token": os.getenv("SHOPIFY_TOKEN"),
+    "printful_api": os.getenv("PRINTFUL_API"),
+    "printful_store_id": os.getenv("PRINTFUL_STORE_ID"),
+    "zapier_webhook": os.getenv("ZAPIER_WEBHOOK"),
+    "github_token": os.getenv("GITHUB_TOKEN"),
+}
